2e09f2905c1578f3629b84e3994ebc1335e595c1
Mathieu Linty <mathieu.linty@studio.unibo.it>
2024-11-26 17:21:37 +0100

    add: pcb branch

--- starterKit/phase1/pcb.c
+++ starterKit/phase1/pcb.c
@@ -1,45 +1,120 @@
 #include "./headers/pcb.h"
+#include "../klog.c"
+
+// MEMSET e MEMCPY
+void *memset (void *dest, register int val, register unsigned int len);
+void *memcpy(void *dest, const void *src, unsigned int len);
+
+void *memset (void *dest, register int val, register unsigned int len)
+{
+  register unsigned char *ptr = (unsigned char*)dest;
+  while (len-- > 0)
+    *ptr++ = val;
+  return dest;
+}
+
+void *memcpy(void *dest, const void *src, unsigned int len)
+{
+  char *d = dest;
+  const char *s = src;
+  while (len--)
+    *d++ = *s++;
+  return dest;
+}
+// ~ MEMSET e MEMCPY
 
 static struct list_head pcbFree_h;
 static pcb_t pcbFree_table[MAXPROC];
 static int next_pid = 1;
 
-void initPcbs() {
+void initPcbs()
+{
+  INIT_LIST_HEAD(&(pcbFree_h));
+  for (int i = 0; i < MAXPROC; i++)
+    list_add(&(pcbFree_table[i].p_list), &pcbFree_h);
 }
 
-void freePcb(pcb_t* p) {
+void freePcb(pcb_t* p)
+{
+  list_add(&(p->p_list), &pcbFree_h);
 }
 
-// Vanno resettati tutti i campi e assegnato un nuovo pid
-pcb_t* allocPcb() {
+pcb_t* allocPcb()
+{
+  if (list_empty(&pcbFree_h)) return NULL;
+
+  // prende il primo elemento di pcbFree
+  struct list_head *newPcb_list = list_next(&pcbFree_h);
+  pcb_t *newPcb = container_of(newPcb_list, pcb_t, p_list);
+
+  // inizializzazione del nuovo pcb
+  list_del(newPcb_list); // stacca il pcb da pcbFree e lo reinizializza
+  newPcb->p_parent = NULL;
+  INIT_LIST_HEAD(&(newPcb->p_child));
+  INIT_LIST_HEAD(&(newPcb->p_sib));
+  newPcb->p_s = (state_t){0};
+  newPcb->p_time = 0;
+  newPcb->p_semAdd = NULL;
+  newPcb->p_supportStruct = NULL;
+  newPcb->p_pid = next_pid++;
+
+  return newPcb;
 }
 
-void mkEmptyProcQ(struct list_head* head) {
+void mkEmptyProcQ(struct list_head* head)
+{
+  INIT_LIST_HEAD(head);
 }
 
-int emptyProcQ(struct list_head* head) {
+int emptyProcQ(struct list_head* head)
+{
+  return head->next == head;
 }
 
-void insertProcQ(struct list_head* head, pcb_t* p) {
+void insertProcQ(struct list_head* head, pcb_t* p)
+{
+  list_add_tail(&(p->p_list), head);
 }
 
-pcb_t* headProcQ(struct list_head* head) {
+pcb_t* headProcQ(struct list_head* head)
+{
+  (void) head;
+  return NULL;
 }
 
-pcb_t* removeProcQ(struct list_head* head) {
+pcb_t* removeProcQ(struct list_head* head)
+{
+  (void) head;
+  return NULL;
 }
 
-pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
+pcb_t* outProcQ(struct list_head* head, pcb_t* p)
+{
+  (void) head;
+  (void) p;
+  return NULL;
 }
 
-int emptyChild(pcb_t* p) {
+int emptyChild(pcb_t* p)
+{
+  (void) p;
+  return 0;
 }
 
-void insertChild(pcb_t* prnt, pcb_t* p) {
+void insertChild(pcb_t* prnt, pcb_t* p)
+{
+  (void) prnt;
+  (void) p;
 }
 
-pcb_t* removeChild(pcb_t* p) {
+pcb_t* removeChild(pcb_t* p)
+{
+  (void) p;
+  return NULL;
 }
 
-pcb_t* outChild(pcb_t* p) {
+pcb_t* outChild(pcb_t* p)
+{
+  (void) p;
+  return NULL;
 }
