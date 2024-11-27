4c57632e6e01338a21515a48ce869480897f8945
Mathieu Linty <mathieu.linty@studio.unibo.it>
2024-11-27 11:36:10 +0100

    add: procQ functions, quasi tutte le procTree functions

--- starterKit/phase1/pcb.c
+++ starterKit/phase1/pcb.c
@@ -68,7 +68,7 @@ void mkEmptyProcQ(struct list_head* head)
 
 int emptyProcQ(struct list_head* head)
 {
-  return head->next == head;
+  return (head != NULL && head->next == head);
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p)
@@ -78,39 +78,63 @@ void insertProcQ(struct list_head* head, pcb_t* p)
 
 pcb_t* headProcQ(struct list_head* head)
 {
-  (void) head;
-  return NULL;
+  if (head->next == head) return NULL;
+  return container_of(head->next, pcb_t, p_list);
 }
 
 pcb_t* removeProcQ(struct list_head* head)
 {
-  (void) head;
-  return NULL;
+  if (emptyProcQ(head)) return NULL;
+  pcb_t *pcb_ptr = container_of(head->next, pcb_t, p_list);
+  list_del(head->next);
+  return pcb_ptr;
 }
 
 pcb_t* outProcQ(struct list_head* head, pcb_t* p)
 {
-  (void) head;
-  (void) p;
+  struct list_head* iter;
+  list_for_each(iter, head){
+    if (iter == &(p->p_list)){
+      list_del(&(p->p_list));
+      return p;
+    }
+  }
   return NULL;
 }
 
 int emptyChild(pcb_t* p)
 {
-  (void) p;
-  return 0;
+  return list_empty(&(p->p_child));
 }
 
+// Devo scorrere tutta la lista di child fino ad arrivare al parent?
 void insertChild(pcb_t* prnt, pcb_t* p)
 {
-  (void) prnt;
-  (void) p;
+  if (emptyChild(prnt)) {
+    INIT_LIST_HEAD(&(prnt->p_child));
+    INIT_LIST_HEAD(&(p->p_child));
+    INIT_LIST_HEAD(&(p->p_sib));
+    list_add_tail(&(p->p_child), &(prnt->p_child));
+  } else {
+    struct list_head *child_list = list_next(&(prnt->p_child));
+    pcb_t *child = container_of(child_list, pcb_t, p_child);
+    list_add_tail(&(p->p_sib), &(child->p_sib));
+  }
+  p->p_parent = prnt;
 }
 
 pcb_t* removeChild(pcb_t* p)
 {
-  (void) p;
-  return NULL;
+  if (p == NULL || emptyChild(p)) return NULL;
+  pcb_t *first_child = container_of(list_next(&(p->p_child)), pcb_t, p_child);
+  if (!list_empty(&(first_child->p_sib))) {
+    pcb_t *first_sibling = container_of(list_next(&(first_child->p_sib)), pcb_t, p_sib);
+    p->p_child.next = &(first_sibling->p_child);
+    list_del(&(first_child->p_sib));
+  }
+  list_del(&(first_child->p_child));
+  first_child->p_parent = NULL;
+  return first_child;
 }
 
 pcb_t* outChild(pcb_t* p)
