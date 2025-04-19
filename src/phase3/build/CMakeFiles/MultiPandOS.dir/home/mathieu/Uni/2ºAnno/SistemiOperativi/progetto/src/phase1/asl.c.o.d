6380fb0f8a9e71c44db69f76d4d5a6c9fc1282c7
Mathieu Linty <mathieu.linty@studio.unibo.it>
2025-04-19 10:12:21 +0200

    Fine phase2. Inizio phase3. Riorganizzazione delle directories

---
+++ src/phase3/build/CMakeFiles/MultiPandOS.dir/home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/asl.c.o.d
@@ -0,0 +1,10 @@
+CMakeFiles/MultiPandOS.dir/home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/asl.c.o: \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/asl.c \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/asl.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/listx.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/types.h \
+ /usr/include/uriscv/types.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/./const.h \
+ /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/./listx.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/pcb.h
---
+++ src/phase3/build/CMakeFiles/MultiPandOS.dir/home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/pcb.c.o.d
@@ -0,0 +1,9 @@
+CMakeFiles/MultiPandOS.dir/home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/pcb.c.o: \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/pcb.c \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/pcb.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/listx.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/types.h \
+ /usr/include/uriscv/types.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/./const.h \
+ /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
+ /home/mathieu/Uni/2ºAnno/SistemiOperativi/progetto/src/phase1/./headers/../../headers/./listx.h
